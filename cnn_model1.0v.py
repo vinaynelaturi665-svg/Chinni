bb146b08e8e72ca6f641b02786b4a6a9f94ebaa2
vinaynelaturi665-svg <vinaynelaturi665@gmail.com>
2025-12-15 15:56:01 +0530

    Add files via upload

---
+++ cnn_model1.0v.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+
+def grad_cam(model, img_array, last_conv_layer_name):
+
+    grad_model = tf.keras.models.Model(
+        [model.inputs],
+        [model.get_layer(last_conv_layer_name).output, model.output]
+    )
+
+    with tf.GradientTape() as tape:
+        conv_outputs, predictions = grad_model(img_array)
+        loss = predictions[:, tf.argmax(predictions[0])]
+
+    grads = tape.gradient(loss, conv_outputs)
+    pooled_grads = tf.reduce_mean(grads, axis=(0,1,2))
+
+    heatmap = tf.reduce_sum(tf.multiply(pooled_grads, conv_outputs), axis=-1)
+    heatmap = np.maximum(heatmap, 0) / np.max(heatmap)
+
+    return heatmap[0]
